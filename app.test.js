4066644956454ede1444778cfff07f4790cb5bf1
Dewan Ishtiaque Ahmed <30604461+dewan-ahmed@users.noreply.github.com>
2024-02-26 15:34:04 -0400

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -1,21 +1,20 @@
 const request = require('supertest');
-const app = require('./app.js'); 
+const { app, server } = require('./app.js');
 
 describe('Express App', () => {
   it('200ok', async () => {
-   const response = await request(app).get('/');
+    const response = await request(app).get('/');
     expect(response.status).toBe(200);
   });
 
   it('correctHTML', async () => {
-     const response = await request(app).get('/');
-    expect(response.text).toContain('Captain Canary\'s Adventures');
+    const response = await request(app).get('/');
+    expect(response.text).toContain("Captain Canary's Adventures");
     expect(response.text).toContain('Pod Name:');
   });
 
   afterAll(() => {
     // Close the server after all tests are complete
-    app.close();
+    server.close();
   });
-  
 });
