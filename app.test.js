1035d65be9f99a44145aee8a8aa09ecbfdda6dff
Dewan Ishtiaque Ahmed <30604461+dewan-ahmed@users.noreply.github.com>
2024-02-26 15:09:12 -0400

    Create app.test.js

---
+++ app.test.js
@@ -0,0 +1,12 @@
+describe('Express App', () => {
+  it('200ok', async () => {
+   const response = await request(app).get('/');
+    expect(response.status).toBe(200);
+  });
+
+  it('correctHTML', async () => {
+     const response = await request(app).get('/');
+    expect(response.text).toContain('Captain Canary\'s Adventures');
+    expect(response.text).toContain('Pod Name:');
+  });
+});
